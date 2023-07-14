2f755e508fc7e809b96172427f94b639bccca598
Fajar Ghifari <fajarghifari3@gmail.com>
2023-07-14 23:01:00 +0700

    Creating a periodic Timer

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,18 +1,42 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:stopwatch/ui/elapsed_time_text_basic.dart';
+import 'package:stopwatch/ui/elapsed_time_text.dart';
 
 class Stopwatch extends StatefulWidget {
   const Stopwatch({Key? key}) : super(key: key);
 
   @override
-  _StopwatchState createState() => _StopwatchState();
+  State<Stopwatch> createState() => _StopwatchState();
 }
 
 class _StopwatchState extends State<Stopwatch> {
+  late DateTime _intialTime;
+  Duration _elapsed = Duration.zero;
+  late final Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _intialTime = DateTime.now();
+    _timer = Timer.periodic(const Duration(milliseconds: 50), (_) {
+      final now = DateTime.now();
+      setState(() {
+        _elapsed = now.difference(_intialTime);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const ElapsedTimeTextBasic(
-      elapsed: Duration(seconds: 5),
+    return ElapsedTimeText(
+      elapsed: _elapsed,
     );
   }
 }
